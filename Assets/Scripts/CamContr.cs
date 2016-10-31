5b36f3b509e14a459c3a09cadd9a3ba78effb570
Danzmann <nathan.danzmann@gmail.com>
2016-10-31 11:57:28 -0200

    Transferindo projeto

---
+++ Assets/Scripts/CamContr.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamContr : MonoBehaviour
+{
+
+    public GameObject Player;
+    public Vector3 Pos;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Pos = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = Player.transform.position + Pos;
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    public Vector3 Rot = new Vector3(15,30,45);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Rotate(Rot * Time.deltaTime);
+	}
+}
