7fd470523633c94cf5ed6f13fb1593bbfc189861
Maglethong Spirr <maglethongspirr@gmail.com>
2016-11-18 09:38:44 -0200

    Otimizando for

--- ScriptText.cs
+++ ScriptText.cs
@@ -6,10 +6,7 @@ public class ScriptText : MonoBehaviour {
     public int i,a;
 	void Start () {
         i = 0;
-        for(i=0;i<10;i++)
-        {
-            a += i;
-        }
+        for(i=9; i>0; i--) a += i;
 	}
 	
 }
