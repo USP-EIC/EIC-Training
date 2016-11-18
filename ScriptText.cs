90ea329ad7f11046e6e1b5695e1e7ab3b52a7d6a
Lucasa22 <lucasa22.lar@gmail.com>
2016-11-18 09:33:56 -0200

    For

---
+++ ScriptText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptText : MonoBehaviour {
+
+    public int i,a;
+	void Start () {
+        i = 0;
+        for(i=0;i<10;i++)
+        {
+            a += i;
+        }
+	}
+	
+}
