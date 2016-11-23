7c9e999f473ef080fdc04de24179eb79ffd9e41b
Danzmann <nathan.danzmann@gmail.com>
2016-11-23 10:15:17 -0200

    no message

---
+++ Assets/0-Scripts/asdasdsad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class asdasdsad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    for(int a = 0; a <10 ;a++)
+	        Debug.Log(a);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
